899a5c0ab4ba3e72584bc7e86da9a5b49b00bf71
alicezero <paopaoboy101@gmail.com>
2017-05-25 15:42:35 +0700

    25-5-60

--- app/src/main/java/pkru/sansak/pongsawat/mypkru/MainActivity.java
+++ app/src/main/java/pkru/sansak/pongsawat/mypkru/MainActivity.java
@@ -1,9 +1,8 @@
 package pkru.sansak.pongsawat.mypkru;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.KeyEvent;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,32 +10,29 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    //explicit ประกาศตัวแปร
+    //Explicit
     private EditText userEditText, passwordEditText;
     private TextView textView;
     private Button button;
-
+    private String userString, passwodString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Initial View
+        //Initial View
         initialView();
 
         //Controller
         controller();
 
 
-    } //main method
+    }   // Main Method
 
     private void controller() {
-
         textView.setOnClickListener(this);
         button.setOnClickListener(this);
-
-
     }
 
     private void initialView() {
@@ -44,25 +40,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         passwordEditText = (EditText) findViewById(R.id.edtPassword);
         textView = (TextView) findViewById(R.id.txtNewRegister);
         button = (Button) findViewById(R.id.btnLogin);
-
     }
-
 
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
 
-        //for Text View
-
-        if (v == textView) {
+        //For TextView
+        if (view == textView) {
             //Intent to NewRegister
-            Intent intent = new Intent(MainActivity.this,NewRegisterActivity.class);
+            Intent intent = new Intent(MainActivity.this, NewRegisterActivity.class);
             startActivity(intent);
-
         }
-        //for Button
-        if (v == button) {
 
+        //For Button
+        if (view == button) {
+            userString=userEditText.getText().toString().trim();
+            passwodString = passwordEditText.getText().toString().trim();
+            if (userString.equals("")||passwodString.equals("")) {
+                //Have Space
+                MyAlert myAlert = new MyAlert(this);
+                myAlert.myDialog(getResources().getString(R.string.titleHaveSpace),
+                getResources().getString(R.string.messageHaveSpace));
+            } else {
+                //No Space
+            }
         }
+
 
     }
-}   //main class
+}   // Main Class นี่คือ คลาสหลัก
